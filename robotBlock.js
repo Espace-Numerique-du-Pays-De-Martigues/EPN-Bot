2fccdc07c1d2b3ebb9b2d5ff3d6382639c123275
Espace-Numerique-du-Pays-De-Martigues <brice.saintmartin@ampmetropole.fr>
2018-04-27 14:49:52 +0200

    Update robotBlock.js

--- robotBlock.js
+++ robotBlock.js
@@ -1376,3 +1376,221 @@ clauseBlock.nextConnection.targetBlock();
 }
 }
 };
+
+Blockly.Blocks['controls_if_color_ligne'] = 
+{
+  helpUrl: 'http://wiki.labaixbidouille.com/index.php/RoboduLAB',
+  /**
+  * Block for if/elseif/else condition.
+  * @this Blockly.Block
+  */
+  init: function() 
+  {
+    this.setColour(210);
+    this.appendValueInput('IF0')
+    .setCheck('bluetooth')
+    .appendField("Si couleur détecté ");
+    this.appendStatementInput('DO0')
+    .appendField("Faire");
+    //.appendField("test 7");
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.setMutator(new Blockly.Mutator(['controls_if_elseif',
+    'controls_if_else']));
+    // Assign 'this' to a variable for use in the tooltip closure below.
+    var thisBlock = this;
+    this.setTooltip(function() 
+    {
+      if (!thisBlock.elseifCount_ && !thisBlock.elseCount_) 
+      {
+        return "Si une valeur est vrai, alors exécuter certains ordres.";
+        //return "test 8";
+      } 
+      else if (!thisBlock.elseifCount_ && thisBlock.elseCount_) 
+      {
+        return "Si une valeur est vrai, alors exécuter le premier bloc d'ordres. Sinon, exécuter le second bloc d'ordres.";
+        //return "test 9";
+      } 
+      else if (thisBlock.elseifCount_ && !thisBlock.elseCount_) 
+      {
+        return "Si la première valeur est vrai, alors exécuter le premier bloc d'ordres. Sinon, si la seconde valeur est vrai, alors exécuter le second bloc d'ordres.";
+        //return "test 10";
+      } 
+      else if (thisBlock.elseifCount_ && thisBlock.elseCount_) 
+      {
+        return "Si la première valeur est vrai, alors exécuter le premier bloc d'ordres. Sinon, si la seconde valeur est vrai, alors exécuter le second bloc d'ordres. Si aucune des valeurs n'est vrai, exécuter le dernier bloc d'ordres.";
+        //return "test 11";
+      }
+      return '';
+    });
+    this.elseifCount_ = 0;
+    this.elseCount_ = 0;
+  },
+  /**
+  * Create XML to represent the number of else-if and else inputs.
+  * @return {Element} XML storage element.
+  * @this Blockly.Block
+  */
+  mutationToDom: function() 
+  {
+    if (!this.elseifCount_ && !this.elseCount_) 
+    {
+      return null;
+    }
+    var container = document.createElement('mutation');
+    if (this.elseifCount_) 
+    {
+      container.setAttribute('elseif', this.elseifCount_);
+    }
+    if (this.elseCount_) 
+    {
+      container.setAttribute('else', 1);
+    }
+    return container;
+  },
+  /**
+  * Parse XML to restore the else-if and else inputs.
+  * @param {!Element} xmlElement XML storage element.
+  * @this Blockly.Block
+  */
+  domToMutation: function(xmlElement) 
+  {
+    this.elseifCount_ = parseInt(xmlElement.getAttribute('elseif'), 10) || 0;
+    this.elseCount_ = parseInt(xmlElement.getAttribute('else'), 10) || 0;
+    for (var i = 1; i <= this.elseifCount_; i++) 
+    {
+      this.appendValueInput('IF' + i)
+      .setCheck('couleur')
+      .appendField("Sinon Si couleur détecté ");
+      this.appendStatementInput('DO' + i)
+      .appendField("Faire");
+      //.appendField("test 13");
+    }
+    if (this.elseCount_) 
+    {
+      this.appendStatementInput('ELSE')
+      .appendField("Sinon");
+      //.appendField("test 14");
+    }
+  },
+  /**
+  * Populate the mutator's dialog with this block's components.
+  * @param {!Blockly.Workspace} workspace Mutator's workspace.
+  * @return {!Blockly.Block} Root block in mutator.
+  * @this Blockly.Block
+  */
+  decompose: function(workspace) 
+  {
+    var containerBlock = Blockly.Block.obtain(workspace, 'controls_if_if');
+    containerBlock.initSvg();
+    var connection = containerBlock.getInput('STACK').connection;
+    for (var i = 1; i <= this.elseifCount_; i++) 
+    {
+      var elseifBlock = Blockly.Block.obtain(workspace, 'controls_if_elseif');
+      elseifBlock.initSvg();
+      connection.connect(elseifBlock.previousConnection);
+      connection = elseifBlock.nextConnection;
+    }
+    if (this.elseCount_) 
+    {
+      var elseBlock = Blockly.Block.obtain(workspace, 'controls_if_else');
+      elseBlock.initSvg();
+      connection.connect(elseBlock.previousConnection);
+    }
+    return containerBlock;
+  },
+  /**
+  * Reconfigure this block based on the mutator dialog's components.
+  * @param {!Blockly.Block} containerBlock Root block in mutator.
+  * @this Blockly.Block
+  */
+  compose: function(containerBlock) 
+  {
+    // Disconnect the else input blocks and remove the inputs.
+    if (this.elseCount_) 
+    {
+      this.removeInput('ELSE');
+    }
+    this.elseCount_ = 0;
+    // Disconnect all the elseif input blocks and remove the inputs.
+    for (var i = this.elseifCount_; i > 0; i--) 
+    {
+      this.removeInput('IF' + i);
+      this.removeInput('DO' + i);
+    }
+    this.elseifCount_ = 0;
+    // Rebuild the block's optional inputs.
+    var clauseBlock = containerBlock.getInputTargetBlock('STACK');
+    while (clauseBlock) 
+    {
+      switch (clauseBlock.type) 
+      {
+        case 'controls_if_elseif':
+          this.elseifCount_++;
+          var ifInput = this.appendValueInput('IF' + this.elseifCount_)
+          .setCheck('couleur')
+          .appendField("Sinon Si couleur détecté ");
+          var doInput = this.appendStatementInput('DO' + this.elseifCount_);
+          doInput.appendField("Faire");
+          //doInput.appendField("test 16");
+          // Reconnect any child blocks.
+          if (clauseBlock.valueConnection_) 
+          {
+            ifInput.connection.connect(clauseBlock.valueConnection_);
+          }
+          if (clauseBlock.statementConnection_) 
+          {
+            doInput.connection.connect(clauseBlock.statementConnection_);
+          }
+          break;
+          
+        case 'controls_if_else':
+          this.elseCount_++;
+          var elseInput = this.appendStatementInput('ELSE');
+          elseInput.appendField("Sinon");
+          // Reconnect any child blocks.
+          if (clauseBlock.statementConnection_) 
+          {
+            elseInput.connection.connect(clauseBlock.statementConnection_);
+          }
+          break;
+        
+        default:
+          throw 'Unknown block type.';
+      }
+      clauseBlock = clauseBlock.nextConnection && clauseBlock.nextConnection.targetBlock();
+    }
+  },
+  /**
+  * Store pointers to any connected child blocks.
+  * @param {!Blockly.Block} containerBlock Root block in mutator.
+  * @this Blockly.Block
+  */
+  saveConnections: function(containerBlock) 
+  {
+    var clauseBlock = containerBlock.getInputTargetBlock('STACK');
+     var i = 1;
+    while (clauseBlock) 
+    {
+      switch (clauseBlock.type) 
+      {
+        case 'controls_if_elseif':
+          var inputIf = this.getInput('IF' + i);
+          var inputDo = this.getInput('DO' + i);
+          clauseBlock.valueConnection_ = inputIf && inputIf.connection.targetConnection;
+          clauseBlock.statementConnection_ = inputDo && inputDo.connection.targetConnection;
+          i++;
+          break;
+
+        case 'controls_if_else':
+          var inputDo = this.getInput('ELSE');
+          clauseBlock.statementConnection_ = inputDo && inputDo.connection.targetConnection;
+          break;
+
+        default:
+          throw 'Unknown block type.';
+      }
+      clauseBlock = clauseBlock.nextConnection && clauseBlock.nextConnection.targetBlock();
+    }
+  }
+};
